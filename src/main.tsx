f0334b605a09b122f9b2dbe7bfd15161f868e927
FYam8 <gentle.green.shade@gmail.com>
2026-08-30 07:27:28 +0900

    Load unified route styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 import './guidedReview.css'
+import './homeRoute.css'
 import { bootstrapSafety } from './safetyBootstrap'
 import SafetyMode from './SafetyMode'
 
