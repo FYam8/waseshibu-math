a5f81f31b0cf982a8e44816c30c1aebbeecd4b35
FYam8 <gentle.green.shade@gmail.com>
2026-08-30 05:33:06 +0900

    Add guided review route

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import MistakeReview from './pages/MistakeReview'
 import Remediation from './pages/Remediation'
 import Fields from './pages/Fields'
 import Reinforcement from './pages/Reinforcement'
+import GuidedReview from './pages/GuidedReview'
 import DataManager from './pages/DataManager'
 import PrepCheck from './pages/PrepCheck'
 
@@ -27,6 +28,7 @@ export default function App() {
         <Route path="/setup-check" element={<PrepCheck />} />
         <Route path="/fields" element={<Fields />} />
         <Route path="/mistakes" element={<MistakeReview />} />
+        <Route path="/guided-review" element={<GuidedReview />} />
         <Route path="/remediate" element={<Remediation />} />
         <Route path="/reinforce" element={<Reinforcement />} />
         <Route path="/practice" element={<Practice />} />
